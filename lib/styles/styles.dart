04597f8bd997938a5806d89d5c4dfd8d6fed6a4d
Nyuol96 <nyuolawach2021@gmail.com>
2024-07-02 10:13:45 +0200

    created styles widget

---
+++ lib/styles/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Styles extends StatelessWidget {
+  const Styles({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
