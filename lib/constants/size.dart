53665797dac3d246e87194f4cd824c806c5d607a
Nyuol96 <nyuolawach2021@gmail.com>
2024-07-04 10:19:14 +0200

    created constants for responsiveness

---
+++ lib/constants/size.dart
@@ -0,0 +1,1 @@
+const double kMindesktopwidth=600.0;
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
 import 'package:my_portfolio/constants/nav_items.dart';
+import 'package:my_portfolio/constants/size.dart';
 import 'package:my_portfolio/styles/styles.dart';
 import 'package:my_portfolio/widgets/header_desktop.dart';
 import 'package:my_portfolio/widgets/header_mobile.dart';
@@ -12,49 +13,48 @@ class Homepage extends StatefulWidget {
   @override
   State<Homepage> createState() => _HomepageState();
 }
-
 class _HomepageState extends State<Homepage> {
   final ScaffoldKey =GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key:ScaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: Drawer(
-         backgroundColor: CustomColor.scaffoldBg,
-         child: ListView(children: [
-          
-         ],),
-      ),
-      body:ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-        //Main
-      // HeaderDesktop(),
-     HeaderMobile(
-      onLogoTap: () {
-        
-      },
-      onMenuTap: () {
-        ScaffoldKey.currentState?.openEndDrawer();
-      },
-     ),
-         //Skills
-        Container(
-          height: 500,
-          width: double.maxFinite,
-          ),
-              //Contact
-        Container(
-          height: 500,
-          width: double.maxFinite,
-          color: Colors.blueGrey,),
-              //Footer
-        Container(
-          height: 500,
-          width: double.maxFinite,
-          )
-      ],),
+    return LayoutBuilder(
+      builder: (context,constraints) {
+        return Scaffold(
+          key:ScaffoldKey,
+          backgroundColor: CustomColor.scaffoldBg,
+          endDrawer:constraints.maxWidth>kMindesktopwidth?null :const Drawer(),
+          body:ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+            // Main
+            if(constraints.maxWidth>kMindesktopwidth)
+          const HeaderDesktop() else
+         HeaderMobile(
+          onLogoTap: () {
+    
+          },
+          onMenuTap: () {
+            ScaffoldKey.currentState?.openEndDrawer();
+          },
+         ),
+             //Skills
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              ),
+                  //Contact
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,),
+                  //Footer
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              )
+          ],),
+        );
+      }
     );
   }
 }
