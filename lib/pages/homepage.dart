d74bdb4a4ae44c46efce645538f2f5f40b76aa75
Nyuol96 <nyuolawach2021@gmail.com>
2024-07-11 11:00:08 +0200

    created platforms and skills widgets

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
 import 'package:my_portfolio/constants/nav_items.dart';
 import 'package:my_portfolio/constants/size.dart';
+import 'package:my_portfolio/constants/skill_items.dart';
 import 'package:my_portfolio/styles/styles.dart';
 import 'package:my_portfolio/widgets/header_desktop.dart';
 import 'package:my_portfolio/widgets/header_mobile.dart';
@@ -55,25 +56,66 @@ class _HomepageState extends State<Homepage> {
               mainAxisSize: MainAxisSize.min,
               children: [
               const Text('What I can Do!',
-              style: TextStyle(fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: CustomColor.whitePrimary),
+              style: TextStyle(
+                fontSize: 24,
+               fontWeight: FontWeight.bold,
+               color: CustomColor.whitePrimary),
               ),
+              const SizedBox(height: 50,),
               //platforms
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Wrap(
-                    children: [
-                      Container(
-                        width: 200,
-                        decoration: BoxDecoration(color: CustomColor.bgLight2,
-                        borderRadius: BorderRadius.circular(5)),
-                        child: ListTile(
-                          leading: Image.asset(''),
-                          title: Text(''),
-                        ),
-                      )
-                    ],
+                  ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 450,
+                    ),
+                    child: Wrap(
+                      spacing: 5.0,
+                      runSpacing: 5.0,
+                      children: [
+                        for(int i=0;i<platformItems.length;i++)
+                        Container(
+                          width: 200,
+                          decoration: BoxDecoration(color: CustomColor.bgLight2,
+                          borderRadius: BorderRadius.circular(5)),
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10),
+                           
+                            leading: Image.asset(platformItems[i]["img"],
+                            width:20,),
+                            title: Text(platformItems[i]["title"]),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 50,),
+                  //skills
+                  Flexible(
+                    child: ConstrainedBox(
+                       constraints: const BoxConstraints(
+                       maxWidth: 500,
+                    ),
+                      child: Wrap(
+                        spacing: 10.0,
+                        runSpacing: 10.0,
+                        children: [
+                          for(int i=0;i<skillItems.length;i++)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 12,
+                            horizontal: 16),
+                          child: Chip(
+                              backgroundColor:CustomColor.bgLight2,
+                            label: Text(skillItems[i]["title"]), 
+                            avatar: Image.asset(skillItems[i]["img"]),),
+                        )
+                      ],),
+                    ),
                   )
                 ],
               )
